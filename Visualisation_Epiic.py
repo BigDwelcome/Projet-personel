b6b9b3ba1f55fd6202875cf2dc4da46983b0fceb
BigDwelcome <douatipascal@gmail.com>
2022-11-18 11:45:03 +0100

    Main 1
    Début du projet

---
+++ Visualisation_Epiic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 08:18:43 2022
+
+@author: douati
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+####### import database
+df= pd.read_excel("Étude d’opinion Xylos (réponses).xlsx")
+
